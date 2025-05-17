d5bf20eaa6ccd74c4b852ebd2685dd1d1c130ae8
Aurora-Pluvia <121271805@qq.com>
2025-05-17 16:17:36 +0800

    CustomCamera

---
+++ src/1_20_CameraCustom/CustomCamera.h
@@ -0,0 +1,137 @@
+#ifndef CUSTOMCAMERA_H
+#define CUSTOMCAMERA_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+//定义摄像机移动的几种可能选项。作为抽象概念使用，以避免使用窗口系统特定的输入方法
+enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+
+// 相机属性初始值
+const float YAW         = -90.0f;
+const float PITCH       =  0.0f;
+const float SPEED       =  2.5f;
+const float SENSITIVITY =  0.1f;
+const float ZOOM        =  45.0f;
+
+class CustomCamera{
+public:
+    // 向量
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+    // 欧拉角
+    float Yaw;
+    float Pitch;
+    // 相机属性
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;//fov
+
+    //构造函数，初始化向量
+    CustomCamera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), 
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) 
+    : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), 
+        MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = position;
+        WorldUp = up;
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
+    }
+    //构造函数，初始化标量
+    CustomCamera(float posX, float posY, float posZ, float upX, 
+        float upY, float upZ, float yaw, float pitch) 
+    : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), 
+        MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = glm::vec3(posX, posY, posZ);
+        WorldUp = glm::vec3(upX, upY, upZ);
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
+    }
+
+    //使用欧拉角和视图矩阵计算lookat视图矩阵
+    glm::mat4 GetViewMatrix(){
+        return glm::lookAt(Position, Position + Front, Up);
+    }
+
+    //键盘操作
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+    {
+        float velocity = MovementSpeed * deltaTime;
+        if (direction == FORWARD)
+            Position += Front * velocity;
+        if (direction == BACKWARD)
+            Position -= Front * velocity;
+        if (direction == LEFT)
+            Position -= Right * velocity;
+        if (direction == RIGHT)
+            Position += Right * velocity;
+        if (direction == UP)
+            Position += Up * velocity;
+        if (direction == DOWN)
+            Position -= Up * velocity;
+    }
+
+    //鼠标移动操作
+    void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
+    {
+        xoffset *= MouseSensitivity;
+        yoffset *= MouseSensitivity;
+
+        Yaw   += xoffset;
+        Pitch += yoffset;
+
+        // 确保鼠标出界时，屏幕不会被翻转
+        if (constrainPitch)
+        {
+            if (Pitch > 89.0f)
+                Pitch = 89.0f;
+            if (Pitch < -89.0f)
+                Pitch = -89.0f;
+        }
+
+        // update Front, Right and Up Vectors using the updated Euler angles
+        updateCameraVectors();
+    }
+
+    //鼠标滚轮操作
+    void ProcessMouseScroll(float yoffset)
+    {
+        Zoom -= (float)yoffset;
+        if (Zoom < 1.0f)
+            Zoom = 1.0f;
+        if (Zoom > 45.0f)
+            Zoom = 45.0f;
+    }
+
+private:
+    // 根据更新后的欧拉角计算相机的方向向量
+    void updateCameraVectors()
+    {
+        // calculate the new Front vector
+        glm::vec3 front;
+        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        front.y = sin(glm::radians(Pitch));
+        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        Front = glm::normalize(front);
+        // also re-calculate the Right and Up vector
+        Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+        Up = glm::normalize(glm::cross(Right, Front));
+    }
+};
+
+#endif
--- src/1_20_CameraCustom/main.cpp
+++ src/1_20_CameraCustom/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "CustomShader.h"
+#include "CustomCamera.h"
 #include <iostream>
 #define STB_IMAGE_IMPLEMENTATION
 #include <tool/stb_image.h>
@@ -12,99 +13,59 @@ using namespace std;
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-//定义相机参数
-glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
-float deltaTime = 0.0f; // 当前帧与上一帧的时间差
-float lastFrame = 0.0f; // 上一帧的时间
-//上一帧的鼠标位置，初始值设置为屏幕中心
+//定义相机
+CustomCamera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
-//定义初始偏航角和俯仰角
-float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
-float pitch =  0.0f;
-//判断鼠标是否是第一次进入窗口
 bool firstMouse = true;
-//视野
-float fov = 45;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, 800, 600);
 }
 
-//修改输入函数实现移动
+//使用封装好的函数
 void processInput(GLFWwindow* window){
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
         glfwSetWindowShouldClose(window, true);
     }
-    //确保不会因为硬件配置影响移动速度
     float cameraSpeed = 2.5f * deltaTime;
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        cameraPos += cameraSpeed * cameraFront;
+        camera.ProcessKeyboard(FORWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        cameraPos -= cameraSpeed * cameraFront;
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+        camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+        camera.ProcessKeyboard(RIGHT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-        cameraPos += cameraSpeed * cameraUp;
+        camera.ProcessKeyboard(UP, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
-        cameraPos -= cameraSpeed * cameraUp;
+        camera.ProcessKeyboard(DOWN, deltaTime);
 }
 
-//监听鼠标移动事件，移动鼠标时执行，xpos和ypos代表鼠标当前的位置
-void mouse_callback(GLFWwindow* window, double xpos, double ypos){
-    //在窗口第一次获取焦点的时候摄像机会突然跳一下
-    //在你的鼠标移动进窗口的那一刻，鼠标回调函数就会被调用，这时候的xpos和ypos会等于鼠标刚刚进入屏幕的那个位置。
-    //这通常是一个距离屏幕中心很远的地方，因而产生一个很大的偏移量，所以就会跳了
-    //使用一个bool变量检验我们是否是第一次获取鼠标输入
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn){
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
     if(firstMouse){
         lastX = xpos;
         lastY = ypos;
         firstMouse = false; 
     }
-
-    //计算当前帧和上一帧鼠标位置的偏移量
     float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos;//计算y时与x相反，因为y坐标是从底部往顶部增大的
+    float yoffset = lastY - ypos;
     lastX = xpos;
     lastY = ypos;
 
-    //定义并应用灵敏度
-    float sensitivity = 0.05f;
-    xoffset *= sensitivity;
-    yoffset *= sensitivity;
-
-    //应用偏移量到俯仰角和偏航角
-    yaw += xoffset;
-    pitch += yoffset;
-    //限制俯仰角不超过89度
-    if(pitch > 89.0f)
-        pitch =  89.0f;
-    if(pitch < -89.0f)
-        pitch = -89.0f;
-    
-    //通过俯仰角和偏航角计算方向向量
-    glm::vec3 front;
-    front.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
-    front.y = sin(glm::radians(pitch));
-    front.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
-    cameraFront = glm::normalize(front);
+    //调用封装好的函数计算
+    camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
-//监听鼠标滚轮事件，鼠标滚轮滚动时执行，yoffset代表鼠标滚轮当前的位置
-//xoffset用于占位，防止注册回调函数时形参数量不兼容
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-    //视野限制在1-45
-    if(fov >= 1.0f && fov <= 45.0f)
-        fov -= yoffset;
-    if(fov <= 1.0f)
-        fov = 1.0f;
-    if(fov >= 45.0f)
-        fov = 45.0f;
+    //调用封装好的函数计算
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
 
 int main(){
@@ -123,20 +84,16 @@ int main(){
         cout << "Failed to initialize GLAD" << endl;
         return -1;
     }
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    //隐藏光标并捕捉光标
-    //捕捉光标表示的是，如果焦点在该程序上，光标应该停留在窗口中（除非程序失去焦点或者退出）
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    //为鼠标操作注册回调函数，移动鼠标时执行mouse_callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
-    //注册滚轮回调函数
     glfwSetScrollCallback(window, scroll_callback);
 
     glEnable(GL_DEPTH_TEST);
 
-    CustomShader myShader("./src/1_19_CameraMove/VertexShader.glsl", "./src/1_19_CameraMove/FragmentShader.glsl");
+    CustomShader myShader("./src/1_20_CameraCustom/VertexShader.glsl", "./src/1_20_CameraCustom/FragmentShader.glsl");
 
     float vertices[] = {
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
@@ -247,7 +204,7 @@ int main(){
     
     while (!glfwWindowShouldClose(window)){
 
-        float currentFrame = glfwGetTime();
+        float currentFrame = static_cast<float>(glfwGetTime());
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
@@ -261,16 +218,12 @@ int main(){
         glActiveTexture(GL_TEXTURE1); 
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        glm::mat4 view;
-        //lookat
-        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-        unsigned int viewLoc  = glGetUniformLocation(myShader.ID, "view");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-        //由于需要调整fov，所以每一帧都需要处理投影矩阵
+        //使用封装好的函数
+        glm::mat4 view = camera.GetViewMatrix();
+        myShader.setMat4("view", view);
         glm::mat4 projection;
-        projection = glm::perspective(glm::radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        unsigned int projectionLoc  = glGetUniformLocation(myShader.ID, "projection");
-        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, &projection[0][0]);
+        projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        myShader.setMat4("projection", projection);
 
         glBindVertexArray(VAO);
         for(unsigned int i = 1; i <= 10; i++){
@@ -278,8 +231,7 @@ int main(){
             model = glm::translate(model, cubePositions[i - 1]);
             float angle = 20.0f * i;
             model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            unsigned int modelLoc = glGetUniformLocation(myShader.ID, "model");
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            myShader.setMat4("model", model);
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
